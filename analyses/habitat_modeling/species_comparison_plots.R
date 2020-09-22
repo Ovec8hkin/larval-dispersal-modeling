4d3f96555d4cb91aae2c5efff1b5aea94ac50087
Joshua Zahner <joshuazahner@gmail.com>
2020-09-21 21:07:15 -0400

    Succinctified code for generating plots for each species using ggplot facets

--- analyses/habitat_modeling/species_comparison_plots.R
+++ analyses/habitat_modeling/species_comparison_plots.R
@@ -1,10 +1,10 @@
-source("../src/gam_functions.R")
+source("../../src/gam_functions.R")
 
-cod_catch_data = get_ecomon_catch_data("gadmor_100m3", "../auxdata/catch-data/ecomon_cod.csv")
-had_catch_data = get_ecomon_catch_data("melaeg_100m3", "../auxdata/catch-data/ecomon_haddock.csv")
-flo_catch_data = get_ecomon_catch_data("limfer_100m3", "../auxdata/catch-data/ecomon_flounder.csv")
-mac_catch_data = get_ecomon_catch_data("scosco_100m3", "../auxdata/catch-data/ecomon_mackerel.csv")
-but_catch_data = get_ecomon_catch_data("pepspp_100m3", "../auxdata/catch-data/ecomon_butterfish.csv")
+cod_catch_data = get_ecomon_catch_data("gadmor_100m3", "../../auxdata/catch-data/ecomon_cod.csv")
+had_catch_data = get_ecomon_catch_data("melaeg_100m3", "../../auxdata/catch-data/ecomon_haddock.csv")
+flo_catch_data = get_ecomon_catch_data("limfer_100m3", "../../auxdata/catch-data/ecomon_flounder.csv")
+mac_catch_data = get_ecomon_catch_data("scosco_100m3", "../../auxdata/catch-data/ecomon_mackerel.csv")
+but_catch_data = get_ecomon_catch_data("pepspp_100m3", "../../auxdata/catch-data/ecomon_butterfish.csv")
 
 cod_catch_data = cbind(cod_catch_data, convert_to_northing_easting(cod_catch_data$lat, cod_catch_data$lon))
 had_catch_data = cbind(had_catch_data, convert_to_northing_easting(had_catch_data$lat, had_catch_data$lon))
@@ -15,59 +15,56 @@ but_catch_data = cbind(but_catch_data, convert_to_northing_easting(but_catch_dat
 cod_catch_data=cbind(cod_catch_data, Species=rep_len("Atlantic Cod", length(cod_catch_data[,1])))
 had_catch_data=cbind(had_catch_data, Species=rep_len("Haddock", length(had_catch_data[,1])))
 flo_catch_data=cbind(flo_catch_data, Species=rep_len("Yellowtail Flounder", length(flo_catch_data[,1])))
-mac_catch_data=cbind(mac_catch_data, Species=rep_len("Atlantcic Mackerel", length(mac_catch_data[,1])))
+mac_catch_data=cbind(mac_catch_data, Species=rep_len("Atlantic Mackerel", length(mac_catch_data[,1])))
 but_catch_data=cbind(but_catch_data, Species=rep_len("American Butterfish", length(but_catch_data[,1])))
 
+cod_catch_data$std_catch = cod_catch_data$catch/sum(cod_catch_data$catch)
+had_catch_data$std_catch = had_catch_data$catch/sum(had_catch_data$catch)
+flo_catch_data$std_catch = flo_catch_data$catch/sum(flo_catch_data$catch)
+mac_catch_data$std_catch = mac_catch_data$catch/sum(mac_catch_data$catch)
+but_catch_data$std_catch = but_catch_data$catch/sum(but_catch_data$catch)
 
-ggplot()+
-  geom_point(data=cod_catch_data, aes(x=sfc_temp,y=catch, colour="cod"), colour="red")+
-  geom_point(data=had_catch_data, aes(x=sfc_temp,y=catch, colour="haddock"), colour="blue")+
-  geom_point(data=flo_catch_data, aes(x=sfc_temp,y=catch, colour="flounder"), colour="green")+
-  geom_point(data=mac_catch_data, aes(x=sfc_temp,y=catch, colour="mackerel"), colour="purple")+
-  geom_point(data=but_catch_data, aes(x=sfc_temp,y=catch, colour="butterfish"), colour="black")
+catch_data = dplyr::bind_rows(cod_catch_data, had_catch_data, flo_catch_data, mac_catch_data, but_catch_data)
+catch_data$Species = factor(catch_data$Species, levels=c("Atlantic Cod", "Haddock", "Yellowtail Flounder", "Atlantic Mackerel", "American Butterfish"))
+
+#ggplot()+
+  geom_point(data=cod_catch_data, aes(x=sfc_temp,y=catch, colour="cod"), colour="red", alpha=0.5)+
+  geom_point(data=had_catch_data, aes(x=sfc_temp,y=catch, colour="haddock"), colour="blue", alpha=0.5)+
+  geom_point(data=flo_catch_data, aes(x=sfc_temp,y=catch, colour="flounder"), colour="green", alpha=0.5)+
+  geom_point(data=mac_catch_data, aes(x=sfc_temp,y=catch, colour="mackerel"), colour="purple", alpha=0.5)+
+  geom_point(data=but_catch_data, aes(x=sfc_temp,y=catch, colour="butterfish"), colour="black", alpha=0.5)
   labs(x="Temperature (˚C)", y="Catch (fish/100m^3)", title = "Catch vs Sea Surface Temperature")
 
-ggplot()+geom_boxplot(data=cod_catch_data, aes(x=Species, y=sfc_temp, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=had_catch_data, aes(x=Species, y=sfc_temp, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=flo_catch_data, aes(x=Species, y=sfc_temp, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=mac_catch_data, aes(x=Species, y=sfc_temp, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=but_catch_data, aes(x=Species, y=sfc_temp, weight=catch), outlier.shape=NA)+
+ggplot()+
+  geom_boxplot(data=catch_data, aes(x=Species, y=sfc_temp, weight=catch, fill=Species), outlier.shape=NA)+
   scale_y_continuous(breaks=round(seq(0, 30, by = 5),1))+
   labs(y="Sea Surface Temperature (˚C)", title="Catch Distribution by Temperature")
 
-ggplot()+geom_boxplot(data=cod_catch_data, aes(x=Species, y=sfc_salt, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=had_catch_data, aes(x=Species, y=sfc_salt, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=flo_catch_data, aes(x=Species, y=sfc_salt, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=mac_catch_data, aes(x=Species, y=sfc_salt, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=but_catch_data, aes(x=Species, y=sfc_salt, weight=catch), outlier.shape=NA)+
+ggsave("../../figs/habitat-models/temp-dist.png", width=10, height=5, units="in", dpi=500)
+
+ggplot()+
+  geom_boxplot(data=catch_data, aes(x=Species, y=sfc_salt, weight=catch, fill=Species), outlier.shape=NA)+
   ylim(25, 35)+
   labs(y="Sea Surface Salinity (psu)", title="Catch Distribution by Salinity")
 
-ggplot()+geom_boxplot(data=cod_catch_data, aes(x=Species, y=depth, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=had_catch_data, aes(x=Species, y=depth, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=flo_catch_data, aes(x=Species, y=depth, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=mac_catch_data, aes(x=Species, y=depth, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=but_catch_data, aes(x=Species, y=depth, weight=catch), outlier.shape=NA)+
+ggsave("../../figs/habitat-models/salt-dist.png", width=10, height=5, units="in", dpi=500)
+
+ggplot()+
+  geom_boxplot(data=catch_data, aes(x=Species, y=depth, weight=catch, fill=Species), outlier.shape=NA)+
   ylim(0, 200)+
   labs(y="Bathymetric Depth (m)", title="Catch Distribution by Depth")
 
-ggplot()+geom_boxplot(data=cod_catch_data, aes(x=Species, y=month, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=had_catch_data, aes(x=Species, y=month, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=flo_catch_data, aes(x=Species, y=month, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=mac_catch_data, aes(x=Species, y=month, weight=catch), outlier.shape=NA)+
-  geom_boxplot(data=but_catch_data, aes(x=Species, y=month, weight=catch), outlier.shape=NA)+
+ggsave("../../figs/habitat-models/depth-dist.png", width=10, height=5, units="in", dpi=500)
+
+ggplot()+
+  geom_boxplot(data=catch_data, aes(x=Species, y=month, weight=catch, fill=Species), outlier.shape=NA)+
   scale_y_continuous(breaks=round(seq(1, 12, by = 1),1))+
   labs(y="Month of Year", title="Catch Distribution by Month")
 
-cod_catch_plot = ggplot(cod_catch_data,aes(x=easting,y=northing, weight=catch))+
-  geom_bin2d(bins=100)+ggtitle("Atlantic Cod Catch")
-had_catch_plot = ggplot(had_catch_data,aes(x=easting,y=northing, weight=catch))+
-  geom_bin2d(bins=100)+ggtitle("Haddock Catch")
-flo_catch_plot = ggplot(flo_catch_data,aes(x=easting,y=northing, weight=catch))+
-  geom_bin2d(bins=100)+ggtitle("Yellowtail Flounder Catch")
-mac_catch_plot = ggplot(mac_catch_data,aes(x=easting,y=northing, weight=catch))+
-  geom_bin2d(bins=100)+ggtitle("Atlantic Mackerel Catch")
-but_catch_plot = ggplot(but_catch_data,aes(x=easting,y=northing, weight=catch))+
-  geom_bin2d(bins=100)+ggtitle("American Butterfish Catch")
+ggplot(catch_data,aes(x=lon,y=lat, weight=std_catch))+
+  geom_bin2d(bins=100)+ggtitle("Catch")+facet_wrap(~Species)+
+  scale_fill_gradient(low="white", high="red")+
+  labs(x="Longitude", y="Latitude", title="Catch Distribution", fill="% catch")
 
-grid.arrange(cod_catch_plot, had_catch_plot, flo_catch_plot, mac_catch_plot, but_catch_plot)
+ggsave("../../figs/habitat-models/geo-dist.png", width=15, height=10, units="in", dpi=500)
+
